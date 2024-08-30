54e0cdbd57daea30c11bdc54bd8fc5addebec66b
Ruth Ayambem <raayambem@gmail.com>
2024-08-29 23:50:31 -0400

    Added headers for all methods.

--- src/main/java/edu/ithaca/dragon/shapes/Rectangle.java
+++ src/main/java/edu/ithaca/dragon/shapes/Rectangle.java
@@ -9,14 +9,23 @@ public class Rectangle {
         this.width = width;
     }
 
+    /**
+     * @return the area of this rectangle
+     */
     public double calcArea(){
        throw new RuntimeException("Not implemented yet");
     }
 
+    /**
+     * @post doubles the size of this rectangle
+     */
     public void doubleSize(){
         throw new RuntimeException("Not implemented yet");
     }
 
+    /**
+     * @return the longest line within this rectangle
+     */
     public double longestLineWithin(){
         throw new RuntimeException("Not implemented yet");
     }
