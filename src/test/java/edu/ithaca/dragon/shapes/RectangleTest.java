468896439b48e228e041bb71853c98f1ba9677fb
Ruth Ayambem <raayambem@gmail.com>
2024-08-29 23:42:22 -0400

    Wrote tests for calcArea in Rectangle. Tests are not passing.

--- src/test/java/edu/ithaca/dragon/shapes/RectangleTest.java
+++ src/test/java/edu/ithaca/dragon/shapes/RectangleTest.java
@@ -8,5 +8,16 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 
 public class RectangleTest {
+    @Test
+    public void calcAreaTest(){
+        Rectangle testRectangle = new Rectangle(2, 6);
+        assertEquals(12, testRectangle.calcArea(), 0.0001);
+
+        Rectangle testRectangle2 = new Rectangle(0.1, 2.222);
+        assertEquals(0.2222, testRectangle2.calcArea(), 0.0001);
+
+        Rectangle testRectangle3 = new Rectangle(0.00001, 0.001);
+        assertEquals(0.00000001, testRectangle3.calcArea(), 0.00000001);
+    }
     
 }
