9e1609015bfc542aaef828e45414c8b5b44fc025
Ruth Ayambem <raayambem@gmail.com>
2024-09-02 20:41:40 -0400

    Wrote tests for calcArea, they are not passing.

--- src/test/java/edu/ithaca/dragon/shapes/TriangleTest.java
+++ src/test/java/edu/ithaca/dragon/shapes/TriangleTest.java
@@ -7,7 +7,14 @@ import org.junit.jupiter.api.Test;
 public class TriangleTest {
     @Test
     public void calcAreaTest(){
+        Triangle testTriangle = new Triangle(2, 4);
+        assertEquals(4, testTriangle.calcArea(), 0.0001);
 
+        Triangle testTriangle2 = new Triangle(5, 1);
+        assertEquals(2.5, testTriangle2.calcArea(), 0.001);
+
+        Triangle testTriangle3 = new Triangle(3.678, 10.452);
+        assertEquals(19.2212, testTriangle3.calcArea(), 0.0001);
     }   
     
     @Test
@@ -17,6 +24,6 @@ public class TriangleTest {
 
     @Test
     public void longestLineTest(){
-        
+
     }
 }
