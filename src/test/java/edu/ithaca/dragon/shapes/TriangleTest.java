3d7f6786d177e828550d193dacfbc328cec6b9f7
Ruth Ayambem <raayambem@gmail.com>
2024-09-02 20:31:23 -0400

    Wrote test headers.

--- src/test/java/edu/ithaca/dragon/shapes/TriangleTest.java
+++ src/test/java/edu/ithaca/dragon/shapes/TriangleTest.java
@@ -1,5 +1,22 @@
 package edu.ithaca.dragon.shapes;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
 public class TriangleTest {
+    @Test
+    public void calcAreaTest(){
+
+    }   
     
+    @Test
+    public void doubleSizeTest(){
+
+    }
+
+    @Test
+    public void longestLineTest(){
+        
+    }
 }
