6590e348b1dd61015893a5c4cb6455a6a1079fab
Ruth Ayambem <raayambem@gmail.com>
2024-09-02 20:21:29 -0400

    Wrote class method headers.

---
+++ src/main/java/edu/ithaca/dragon/shapes/Triangle.java
@@ -0,0 +1,31 @@
+package edu.ithaca.dragon.shapes;
+
+public class Triangle {
+    private double triangleHeight;
+    private double triangleBase;
+
+    public Triangle(double height, double base) {
+        triangleHeight = height;
+        triangleBase = base;
+    }
+
+    /*
+     * @return the area of this triangle
+    */
+    public double calcArea(){
+        return 0.0;
+    }
+
+    /*
+     * @post double the size of the triangle
+     */
+    public void doubleSize(){
+    }
+
+    /*
+     * @return the longest line in the triangle
+     */
+    public double longestLineWithin(){
+        return 0.0;
+    }
+}
