179ab98c4ca85a44be58ae6a39ca9815e30e5c49
Ruth Ayambem <raayambem@gmail.com>
2024-09-02 22:48:04 -0400

    Created rectangleList, and 5 random rectangles

--- src/main/java/edu/ithaca/dragon/shapes/ShapeMain.java
+++ src/main/java/edu/ithaca/dragon/shapes/ShapeMain.java
@@ -1,10 +1,23 @@
 package edu.ithaca.dragon.shapes;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import java.util.Scanner;
+
+
 public class ShapeMain {
     
     public static void main(String[] args){
         //Make a list of 5 randomly-sized rectangles and print their area and the largest line that can be drawn through them
         //Allow the user to choose one, double the size of that one, and print them all again
         //Use a loop to repeat the process 5 times
+
+       List<Rectangle> rectangleList = new ArrayList<>();
+
+        for (int i = 0; i < 5; i++){
+            Rectangle newRectangle = new Rectangle((Math.random() * 50), (Math.random() * 50));
+            rectangleList.add(newRectangle);
+        }
     }
 }
