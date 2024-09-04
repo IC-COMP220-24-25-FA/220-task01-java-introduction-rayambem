ac24cc2586d84963bec87ddd7205e7ad593c4c52
Ruth Ayambem <raayambem@gmail.com>
2024-09-04 00:26:19 -0400

    Added comments to LargestOfThreeTest()

--- src/test/java/edu/ithaca/dragon/practice/FunctionPracticeTest.java
+++ src/test/java/edu/ithaca/dragon/practice/FunctionPracticeTest.java
@@ -12,11 +12,15 @@ public class FunctionPracticeTest {
 
     @Test
     public void largestOfThreeTest(){
+        // Checks that the positioning of arguments does not affect largestOfThree() results
         assertEquals(5, FunctionPractice.largestOfThree(4, 5, 2));
         assertEquals(5, FunctionPractice.largestOfThree(5, 4, 2));
         assertEquals(5, FunctionPractice.largestOfThree(4, 2, 5));
+
+        // Accounts for any confusion from having the same value for all arguments
         assertEquals(5, FunctionPractice.largestOfThree(5, 5, 5));
 
+        // Checks that the right exception is thrown when invalid arguments are used - no matter where they are placed
         assertThrows(IllegalArgumentException.class, () -> FunctionPractice.largestOfThree(-1, -1, -1));
         assertThrows(IllegalArgumentException.class, () -> FunctionPractice.largestOfThree(5, -1, 3));
         assertThrows(IllegalArgumentException.class, () -> FunctionPractice.largestOfThree(5, 3, -1));
